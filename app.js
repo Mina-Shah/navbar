12b00108e271245a5603d526b2a2620cea7c4323
Mina-Shah <shahmina814@gmail.com>
2024-08-18 13:19:56 +0500

    added toggle

--- app.js
+++ app.js
@@ -8,10 +8,13 @@ const navToggle = document.querySelector('.nav-toggle');
 const links = document.querySelector('.links');
 
 navToggle.addEventListener('click', function () {
-    if (links.classList.contains('show-links')) {
-        links.classList.remove('show-links')
-    } else {
-        links.classList.add('show-links')
-    }
+    // if (links.classList.contains('show-links')) {
+    //     links.classList.remove('show-links')
+    // } else {
+    //     links.classList.add('show-links')
+    // } 
+    links.classList.toggle('show-links');
 })
 
+//The toggle() method toggles between hide() and show() for the selected elements.
+//The Classlist in JavaScript property allows you to powerfully manipulate the classes(the group of HTML elements) attached to an HTML Element.
